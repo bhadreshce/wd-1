a0daba758e2b4be9f933e7df6bf761b1f42006ee
bhadreshce <bhadreshce17@gmail.com>
2023-12-11 14:16:57 +0530

    add

---
+++ event.js
@@ -0,0 +1,21 @@
+var rev = ['aabbc', 'bbca', 'nnnnsa', 'eweweweaaa']
+var newArr = []
+let len = rev.length
+for (var i = 0; i < len; i++) {
+  //   let pop = rev.pop()
+  //   console.log(pop)
+
+  //   newArr[i] = pop
+//       count += 1
+//     }
+//   }
+//   countArray[i] = count
+//   count = 0
+// }
+// var sum = 0
+// for (var k = 0; k < countArray.length; k++) {
+//   sum += countArray[k]
+// }
+
+// console.log(countArray)
+// console.log(sum)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+  newArr[i] = pop
