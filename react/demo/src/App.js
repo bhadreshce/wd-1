bd2a7a6b8a86b60d3e78b56d42f660113635aaec
bhadreshce <bhadreshce17@gmail.com>
2024-02-13 15:06:54 +0530

    22

---
+++ react/demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Head from './Head'
+function App() {
+  return (
+    <>
+      <Head />
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore praesentium, non aperiam impedit accusamus incidunt qui, illum pariatur, nam sit nemo fugit ut eos amet ducimus magnam. Odio, dicta eius!</h1>
+    </>
+  );
+}
+
+export default App;
