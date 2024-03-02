0a699d721bd8a38474e05300e4edf3201ac4a86b
bhadreshce <bhadreshce17@gmail.com>
2024-03-02 15:05:25 +0530

    add

--- react/demo/src/Todo.js
+++ react/demo/src/Todo.js
@@ -3,36 +3,52 @@ import React,{ useState ,useRef } from 'react'
 
 function Todo(){
    const [todo, setTodo ] = useState([])
+   const [index, setIndex ] = useState(null)
 const input = useRef(null)
-	const AddData = () =>{
-       setTodo([...todo, input.current.value ])
+	const AddData = () => {
+    if(index == null){
+            setTodo([...todo, input.current.value ])
        input.current.value = ''
+    }else{
+        let uData =  [...todo]
+         uData[index] = input.current.value
+          setTodo(uData)
+    }
+      
       
 	}
 
-	const deleteData = (index) =>{
-                let newdata = todo.filter((result,id)=>{
-                  return id != index
-               })
-                
-                setTodo(newdata)
-	}
+	const deleteData = (i) => {
+       let newTodo = todo.filter((result , index )=>{
+             return index != i
+      })
 
-   return <>
+       setTodo(newTodo)
+  }
+
+  const updateData = (result,i) =>{
+    input.current.value = result
+    setIndex(i)
+  }
+
+   return <>  
    <center>
       <input type='text' ref={input} />
       <button onClick={()=>{
       	 AddData()
       }}>
-        Add
+       { index==null? 'Add': 'Update'}
       </button>
       {todo.map((result,i)=>{
       	return (
           <>
           <h1>{result}</h1>
           <button onClick={()=>{
-          	deleteData(i)
+            deleteData(i)
           }}>Delete</button>
+          <button onClick={()=>{
+              updateData(result,i)
+          }}>Update</button>
           </>
       	)
       })}
