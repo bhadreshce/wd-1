f51d0ed92ab9d0b5e9e57e5bcf2d905543631dbf
bhadreshce <bhadreshce17@gmail.com>
2024-03-14 14:31:14 +0530

    add

--- react/test/src/effect/ToDo.js
+++ react/test/src/effect/ToDo.js
@@ -4,6 +4,9 @@ import React, { useState, useEffect, useRef } from 'react'
 
 const ToDo = () => {
     const [data, setData] = useState([])
+    const [index, setIndex] = useState(null)
+
+
     const inpt = useRef(null)
     useEffect(() => {
         getData()
@@ -17,11 +20,32 @@ const ToDo = () => {
     }
 
     const addData = () => {
-        axios.post('http://localhost:8001/std', { name: inpt.current.value }).then(() => {
+        if (index == null) {
+            axios.post('http://localhost:8001/std', { name: inpt.current.value }).then(() => {
+                getData()
+                inpt.current.value = null
+            })
+        } else {
+
+            axios.patch(`http://localhost:8001/std/${index}`, { name: inpt.current.value }).then((res) => {
+                getData()
+                inpt.current.value = null
+                setIndex(null)
+            })
+
+        }
+
+    }
+
+    const deleteData = (i) => {
+        axios.delete(`http://localhost:8001/std/${i}`).then(() => {
             getData()
         })
+    }
 
-
+    const updateData = (id, name) => {
+        setIndex(id)
+        inpt.current.value = name
     }
 
 
@@ -33,17 +57,26 @@ const ToDo = () => {
                 <input type="text" ref={inpt} />
                 <button onClick={() => {
                     addData()
-                }}>Add</button>
+                }}>{index == null ? 'Add' : 'Update'}</button>
                 <table border={1} rules='all'>
                     <tr>
-
+                        <th>id</th>
                         <th>Name</th>
+                        <th>Action</th>
                     </tr>
-                    {data.map((result) => {
+                    {data.map((result, i) => {
                         return (
                             <>
                                 <tr>
-                                    <td>{result.name}</td>
+                                    <td>{i + 1}</td>
+                                    <td>{result.name} </td>
+                                    <td><button onClick={() => {
+                                        deleteData(result.id)
+                                    }}>Delete</button>
+                                        <button onClick={() => {
+                                            updateData(result.id, result.name)
+                                        }}>Update</button>
+                                    </td>
                                 </tr>
                             </>
                         )
