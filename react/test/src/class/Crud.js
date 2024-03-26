a104a57d47ff3d953392ac47f2ba0d0af3ece7c4
bhadreshce <bhadreshce17@gmail.com>
2024-03-26 14:26:13 +0530

    add

--- react/test/src/class/Crud.js
+++ react/test/src/class/Crud.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function Crud() {
     const [input, setInput] = useState({ name: '', email: '', phone: '' });
+    const [id, setId] = useState(null)
     const [data, setData] = useState([])
-
+    const name = useRef('')
+    const email = useRef('')
+    const phone = useRef('')
     useEffect(() => {
         console.log(input);
     }, [input])
@@ -21,25 +24,85 @@ function Crud() {
 
     const handlleSubmit = (e) => {
         e.preventDefault();
-        setData([...data, input])
 
+        if (id == null) {
+            setData([...data, input])
+        } else {
+            let upData = [...data]
+            upData[id].name = name.current.value
+            upData[id].email = email.current.value
+            upData[id].phone = phone.current.value
+            setData(upData)
+
+        }
+
+        setInput({ name: '', email: '', phone: '' })
+        name.current.value = ''
+        email.current.value = ''
+        phone.current.value = ''
+        setId(null)
     }
 
+    const deleteData = (i) => {
+        let newData = [...data]
+        newData.splice(i, 1)
+        setData(newData)
+    }
 
+    const updateData = (res, index) => {
+        setId(index)
+        name.current.value = res.name
+        email.current.value = res.email
+        phone.current.value = res.phone
+    }
     return (
         <>
             <form onSubmit={handlleSubmit}>
                 <label>Name</label>
-                <input type="text" defaultValue={input.name} onKeyUp={handlleChange} name="name" id="" />
+                <input type="text" ref={name} onKeyUp={handlleChange} name="name" id="" />
                 <br />
                 <label>Email</label>
-                <input type="email" defaultValue={input.email} onKeyUp={handlleChange} name="email" id="" />
+                <input type="email" ref={email} onKeyUp={handlleChange} name="email" id="" />
                 <br />
                 <label>Phone</label>
-                <input type="text" defaultValue={input.phone} onKeyUp={handlleChange} name="phone" id="" />
+                <input type="text" ref={phone} onKeyUp={handlleChange} name="phone" id="" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+            <br />
+            <br />
+            <br />
+            <center>
+
+                <table border={1} rules='all'>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                    {data.map((result, index) => {
+                        return (
+                            <>
+                                <tr>
+                                    <td>{result.name}</td>
+                                    <td>{result.email}</td>
+                                    <td>{result.phone}</td>
+                                    <td><button onClick={() => {
+                                        deleteData(index)
+                                    }}>Delete</button>
+                                        <button onClick={() => {
+                                            updateData(result, index)
+                                        }}>Update</button>
+                                    </td>
+
+                                </tr>
+                            </>
+                        )
+                    })}
+                </table>
+            </center>
+
         </>
     );
 }
